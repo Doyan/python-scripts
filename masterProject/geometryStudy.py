80a1e7649ca4221c7e285c4edb9dcb2b493a3b5e
Doyan <gabrielamandus@gmail.com>
2018-02-19 18:22:36 +0100

    precis startat geometriuppskattning

---
+++ masterProject/geometryStudy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 19 13:36:53 2018
+
+@author: gabgus
+"""
+
+import numpy as np, matplotlib.pyplot as plt
+
+#------------------------------------------------------------------------------
+
