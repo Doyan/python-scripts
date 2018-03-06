92a4090418c33f9289a08ddc22be2fc2352f582a
Doyan <gabrielamandus@gmail.com>
2018-03-06 09:40:27 +0100

    cantera exempel inlagt

--- masterProject/geometryStudy.py
+++ masterProject/geometryStudy.py
@@ -5,8 +5,13 @@ Created on Mon Feb 19 13:36:53 2018
 
 @author: gabgus
 """
+import sys
+sys.path.append('/chalmers/users/gabgus/miniconda2/envs/p3/lib/python3.5/site-packages/cantera/')
+
 
 import numpy as np, matplotlib.pyplot as plt
-
+import cantera as ct
 #------------------------------------------------------------------------------
-
+g = ct.Solution('gri30.xml')
+g.TPX = 300.0, ct.one_atm, 'CH4:0.95,O2:2,N2:7.52'
+g.equilibrate('TP')
