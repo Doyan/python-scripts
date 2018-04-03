2dbe8883a3149e50d41a4efe937f309b5aaddd6f
Doyan <gabrielamandus@gmail.com>
2018-04-03 17:33:35 +0200

    börjat på case-struktur

---
+++ masterProject/fuels-gas.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  3 11:39:15 2018
+
+@author: gabgus
+"""
+import numpy as np
+# -------------------------
+# Collection of fuels and corresponding gas yields
+
+LHVi = np.array([283.0, 286.0, 0, 891.0, 1411.0, 0])     # kj/mol gas components
+MWi  = [28.01, 2.016, 44.01, 16.043, 28.053, 18.015] # g/mol
+MWi = np.array(MWi) / 1000 # kg / mol
+
+MW_CHO = np.array([12.01, 1.007, 15.998])/1e3 # kg/mol
+
+
+# CHALMERS DFB Gasifier
+
+case_chalmers = {}
+
+# Input
+
+# [co, h2, co2, ch4, c2h4, h2o]
+ni_cg =  np.array([12.1, 7.5, 4.05, 4.1, 1.6, 0])
+CHONS = np.array([50.55,6.13,43.22,0.07,0.01]) / 100
+SB = 0.7
+ER = 1.2
+LHV_fuel = 19.03 # MJ/kg daf
+Ych = 0.16
+
+
+# derived
+
+CHONS = CHONS/CHONS.sum()
+ 
+yGas_mass = np.sum(ni_cg * MWi) # kg / kg daf
+X_cg = ni_cg/ni_cg.sum()
+
+
+MW_cg = np.sum(X_cg * MWi) # kg / mol gas
+LHV_cg_mole = np.sum(X_cg*LHVi) # kj/mol
+
+LHV_cg_mass = LHV_cg_mole/MW_cg
+
+
+nCHO_fuel = CHONS[0:3] / MW_CHO
+xCHO_fuel = nCHO_fuel/nCHO_fuel.sum()
+MW_fuel = np.sum(xCHO_fuel*MW_CHO)
+
+
+# flue gas and air
+CHO = CHONS[0:3]
+
+ni_fuel = CHO / MW_CHO
+
+
+
+co2 = ni_fuel[0]
+h2o = ni_fuel[1] / 2
+o2 = co2 + h2o/2 - ni_fuel[2]/2
+
+ni_fg = np.array([co2, h2o, o2*(ER-1), o2*ER*3.76])
+ni_air = o2*ER + o2*ER*3.76
+
+MW_O2 = 15.998*2/1000
+MW_N2 = 14.0067*2/1000
+
+m_air = o2*ER*MW_O2 + o2*ER*3.76*MW_N2
+
+
+
+
+case_chalmers['name'] = 'Chalmers'
+case_chalmers['ni'] = ni_cg
+case_chalmers['X_cg'] = X_cg
+case_chalmers['yGas_mass'] = yGas_mass
+case_chalmers['yGas_mole'] = ni_cg.sum()
+case_chalmers['CHONS'] = CHONS
+case_chalmers['yield_char'] = Ych 
+case_chalmers['SB'] = SB
+case_chalmers['ER'] = ER
+case_chalmers['AFR'] = m_air
+case_chalmers['LHV_fuel'] = LHV_fuel
+case_chalmers['LHV_cg_mass'] = LHV_cg_mass
+case_chalmers['LHV_cg_mole'] = LHV_cg_mole
+
+def saveCase(casedict,fnam):
+    np.save(fnam,casedict)
+    return
+
+def readCase(fnam):
+    data = np.load(fnam).flat[0]
+    header = ''
+    delim = ' '
+    for k, v in data.items():
+        header = header + delim + k
+    print('Case: ' + data['name'] + '. -- Loaded variables:\n')
+    print(header)
+    return data, header
+    
+    
+
+def getHeader(case):
+    header = ''
+    delim = ' '
+    data = []
+    for k, v in case.items():
+        header = header + delim + k
+        data.append(v)
+    print(header)
+    print(data)     
+
+
+saveCase(case_chalmers,'case_chalmers')
+
--- masterProject/readcalc.py
+++ masterProject/readcalc.py
@@ -1,14 +1,26 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Mar 23 13:32:55 2018
+Created on Tue Apr  3 13:52:51 2018
 
-@author: Gabriel
+@author: gabgus
 """
-
+# ---------------------------------------------------------------------
 import numpy as np
 from thermopy import nasa9polynomials as nasa9
 db=nasa9.Database()
 p_atm = 0.101325 # MPa
+
+# read fuel - gas variables from file
+def readCase(fnam):
+    data = np.load(fnam).flat[0]
+    header = ''
+    delim = ' '
+    for k, v in data.items():
+        header = header + delim + k
+    print('Case: ' + data['name'] + '. -- Loaded variables:\n')
+    print(header)
+    return data, header
 
 # ---------------------------------------------------------------------
 # Input variables
@@ -16,9 +28,7 @@ xH2O_G = 0.1     # Moisture content in Gasification feedstock
 xH2O_C = 0.2     # Moisture content in Combustion fuel
 
 Xch=0.75      # char conversion
-Xvol=0.1299     # fraction of heat from combusting volatiles
 
-SB = 0.6    # Steam-Biomass ratio
 ER = 1.2    # Equivalence ratio
 
 # Effects in the gasifier and combustor
@@ -40,33 +50,16 @@ Tf = 80      # fuel temp, total guess        -- check sensitivity
 Tair = 200   # air temp,                     -- check sensitivity
 
 Tevap=100   # Boiling point of water
+Tpyro=400   # Temp at which fuel dissociates
+
 
 Tref = 25   # Reference temp
 TK=273.15   # for conversion to Kelvin
 
-yield_gas = 0.91 # kg cold gas per kg fuel
 
+case,header = readCase('case_chalmers.npy')
 
-# Fuel CHONS chalmers dfb
-
-# Assume ni based on chalmers DFB at SB=0.7
-# co h2 co2 ch4 c2h4  
-ni_cg=np.array([12.1, 7.5, 4.05, 4.1, 1.5]) # mol/kg daf fuel
-
-# Given product composition
-# [co, h2, co2, ch4, c2h4, h2o]
-X_gas_dry = np.array([0.358, 0.304, 0.166, 0.137, 0.03, 0])
-X_gas_dry = X_gas_dry/X_gas_dry.sum()
-
-# Elemental composition of fuel
-Xc_fuel = np.array([1.0, 1.37, 0.61]) # normalised for carbon
-X_fuel = Xc_fuel/Xc_fuel.sum()     # normalised to 1 mol fuel
-
-# Molar weight of elements and fuel
-MW_CHO = np.array([12.01, 1.007, 15.998])/1e3 # kg/mol
-MW_fuel = np.sum(X_fuel * MW_CHO) # kg/mol
-
-
+# -------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 # Table values
 
@@ -150,112 +143,65 @@ def senseHmix(comps,X,T):
     for i, comp in enumerate(comps):
         h.append(X[i] * sensHeat(comp,T))
     return sum(h)
-    
-# -----------------------------------------------------------------------------
-# 
-    
-# readCase
+
+# -------------------------------------------------------------------------    
+# mass flows 
+
+Pfuel_c = (Pgas+Pheat)/nboil
+
+m_cg = Pgas / case['LHV_cg_mass']
+
+mf_g = m_cg / case['yGas_mass']
+m_steam = mf_g*case['SB']
 
 
-# Mass flows 
-    
-# get LHV of cold gas and from that massflow of cold gas
-LHV_gas = LHVgas(X_gas_dry)
-m_cg = Pgas/LHV_gas
-
-# massflow of fuel needed to generate said gas
-mf_g = m_cg/yield_gas
-
-# massflow of fuel needed to generate the total needed heat, 
-# boiler efficiency of 0.89
-mf_c = (Pheat + Pgas)/nboil/LHV_f
-nf_c  = mf_c/MW_fuel
+mf_c = Pfuel_c/case['LHV_fuel']
+m_air = mf_c*case['AFR']
 
 # Convert wet basis moisture content to dry basis
 xH2O_C = makeDryBasis(xH2O_C)
 xH2O_G = makeDryBasis(xH2O_G)
 
-# Fetch total amount of water from moisture in each chamber
 mm_c = xH2O_C*mf_c
 mm_g = xH2O_G*mf_g
 
+mchar_c = mf_c*case['yield_char'] 
+mchar_g = mf_g*case['yield_char'] 
 
-# amount and comp of dry fluegas per mol fuel
-X_fg, yfg, yAir = fg_comp(X_fuel,ER)
-MW_fg = mixtureMW(X_fg,comps_flue)
 
-MW_air = mixtureMW(X_air,comps_air)
 
-m_fg = nf_c*yfg*MW_fg 
-m_air = nf_c*yAir*MW_air
 
-# Mass steam leaving with gas
-steam_reacted =  6.5*MW_h2o/(96*MW_fuel) # kg steam reacted per kg fuel
-ms_g = mf_g * (SB - steam_reacted)
-
-# -----------------------------------------------------------------------------
-# Heat calculations
-
-# Heat needed due to 1kg moisture
-qh2o=(sensHeat(h2ol,Tf) - sensHeat(h2ol,Tevap) - dHvap/MW_h2o/1e3) # kj/ kg h2o(l)
-
-# Heat entering with 1 kg air
-qair = senseHmix(comps_air,X_air,Tair) # kj/kg air
-
-# Heat entering with 1 kg steam
-qsteam = sensHeat(h2o, Ts)  # kj/kg steam
-
-# Fetch good cp for gas and flue
-Cp_g = meanCpmix(comps_gas,X_gas_dry,Tref,TG) # kj/kgK
-Cp_fg = meanCpmix(comps_flue,X_fg,Tref,TC)
-Cp_m = meanCp(h2o,Tref,TC)
+# -------------------------------------------------------------------------
+# Heat sinks 
 
 # Cp for fuel
 Cp_f = (5.46*(Tf+Tref+2*TK)/2 - 524.77)/1000 # kj/kgK fuel Gupta 2003
 
-# Reaction enthalpy per kg daf fuel
-dHr = 131.48 # kJ/mol  reaction: C(s) + H2O -> CO + H2
+# Cp for gas
+Cp_g = meanCpmix(comps_gas,case['X_cg'],Tref,TG)
 
-qrchar= pa_f[1]/C.molecular_weight *Xch*dHr # kj/kg
+# Cp for char j/kgK fuel Gupta 2003
+Cp_ch = -0.0038*((Tpyro + TG +2*TK)/2)**2 + 5.98*(Tpyro + TG + 2*TK)/2 - 795.28
+Cp_ch = Cp_ch/1000 # kj/kgK
 
-# Energy from combustion of char per kg daf fuel
-qcchar = pa_f[1] * LHV_ch*1e3
+# needed to heat 1kg of daf fuel to dissociation
+qfuel = Cp_f*(Tf - Tref) - Cp_f * (Tpyro -Tref) #kj/kg
 
-# energy from combusting volatiles
-qcvola = (LHV_f*1e3 - qcchar ) * Xvol
+# needed heat to heat 1kg char to Bed temp
+qchar = Cp_ch*(Tpyro - Tref) - Cp_ch*(TG - Tref) #kj/kg
 
-# energy from gasification char
-bonus = pa_f[1]*(1-Xch)*LHV_ch
+# needed to heat and evaporate 1kg moisture 
+qh2o=(sensHeat(h2ol,Tf) - sensHeat(h2ol,Tevap) - dHvap/MW_h2o/1e3) #kj/kg
 
-## -------------------- Actual source terms ------------------------------------
 
-# Boundary conditions
-Q_air = qair*m_air # kW
-Q_steam = qsteam*mf_g*SB # kW
-mcp_C = (m_fg*Cp_fg + mm_c*Cp_m) # kW/K 
-mcp_G = (m_cg*Cp_g + mm_g*Cp_m) # kW/K
 
-# Volumetric terms
-Q_C = mf_c * Cp_f*(Tf-Tref) + mf_c*qcchar - mm_c*qh2o + mf_g*bonus + mf_c*qcvola # kW
-Q_G = mf_g * Cp_f*(Tf-Tref) - mf_g*qrchar - mm_g*qh2o  # kW
 
-# total balance to see if more radiation is needed
-Qsum = Q_air + Q_steam + Q_C + Q_G - mcp_C*(TC-Tref) - mcp_G*(TG-Tref)
 
-# ---- Printing ------------
-print('\n\n===================================================================\n')
-print('Final input terms:\n')
 
-print('Q_G = {:.5} MW'.format(Q_G/1e3))
-print('Q_C = {:.5} MW\n'.format(Q_C/1e3))
 
-print('qair = {:.5} MW'.format(Q_air/1e3))
-print('qsteam = {:.5} MW\n'.format(Q_steam/1e3))
-print('mCP_flue = {:.5} kW/K'.format(mcp_C))
-print('mCP_gas = {:.5} kW/K\n'.format(mcp_G))
 
-print(Xvol)
-print(Qsum)
+
+
 
 
 
