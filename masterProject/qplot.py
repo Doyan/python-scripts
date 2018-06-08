11251e69e331bfa770d1495a7b335041ac41037f
Doyan <gabrielamandus@gmail.com>
2018-06-08 15:27:29 +0200

    included more data, made qplot ore versatile

--- masterProject/qplot.py
+++ masterProject/qplot.py
@@ -16,15 +16,27 @@ fnam ='Qbed.csv'
 data = pd.read_csv(path+fnam,sep=',',skiprows=1, names=['time','q'] )
 
 #%%
-
+# Config
 N = data.shape[0]
+tstart=10
+Rx = 0.7
 
-qframe = data.q.iloc[0:N]
-tframe = data.time.iloc[0:N]
+
+# Index generation
+start=pd.Index(data.time).get_loc(tstart,'pad')
+
+Rn = (N - start)*Rx
+
+
+# data packaging
+qframe = data.q.iloc[start:N]
+tframe = data.time.iloc[start:N]
 
 t = np.array(tframe)
 q = np.array(qframe)
 
+
+# Average creations
 qsum =qframe.cumsum()
 qavg= qframe.expanding().mean()
 
@@ -33,6 +45,11 @@ trev = tframe.sort_index(ascending=False, axis=0)
 
 qavgr = qrev.expanding().mean()
 
+qroll=qframe.rolling(int(np.ceil(Rn))).mean()
+qrollr = qrev.rolling(int(np.ceil(Rn))).mean()
+
+
+
 avgrdiff = qavgr.diff() 
 
 plt.figure(0, figsize=(4,4))
@@ -40,7 +57,7 @@ plt.plot(t,qframe)
 plt.xlabel('Time [s]')
 plt.ylabel('Net energy flow [W]')
 plt.title('Raw monitor signal')
-plt.xlim(7.9,29.2)
+plt.xlim(t[0]-0.1,t[-1]+0.1)
 plt.grid()
 plt.savefig('raw.pdf', bbox_inches='tight')
 
@@ -53,21 +70,27 @@ plt.legend(loc='best')
 plt.xlabel('Time [s]')
 plt.ylabel('Average net energy flow [W]')
 plt.title('Expanding time average')
-plt.xlim(7.9,29.2)
+
+plt.xlim(t[0]-0.1,t[-1]+0.1)
 plt.savefig('cumavg.pdf', bbox_inches='tight')
 
 
 plt.figure(2,figsize=(8,5))
 plt.plot(t,qavg,label='Expanding average')
 plt.plot(trev,qavgr,label='Reverse expanding average')
-plt.plot([7,29],[555000,555000],'--',color=(0.5,0.5,0.5),label='Expected lower bound')
-plt.plot([7,29],[2777000,2777000],'-.',color=(0.5,0.5,0.5),label='Expected higher bound')
+plt.plot(t,qroll,label='Rolling average')
+#plt.plot(trev,qrollr,label='Reverse rolling average')
+
+
+
+plt.plot([t[0],t[-1]+0.1],[555000,555000],'--',color=(0.5,0.5,0.5),label='Expected lower bound')
+plt.plot([t[0],t[-1]+0.1],[2777000,2777000],'-.',color=(0.5,0.5,0.5),label='Expected higher bound')
 
 plt.xlabel('Time [s]')
 plt.ylabel('Average net energy flow [W]')
 plt.title('Expanding time average')
 
-plt.xlim(8.01,29.2)
+plt.xlim(t[0]-0.1,t[-1]+0.1)
 plt.ylim(-0.8e7,0.8e7)
 plt.grid()
 plt.legend(loc='best')
@@ -81,7 +104,7 @@ plt.grid()
 plt.xlabel('Time [s]')
 plt.ylabel('Cumulative energy flow [W]')
 plt.title('Net accumulation')
-
+plt.xlim(t[0]-0.1,t[-1]+0.1)
 print(qavg.iloc[-1] * 1.4 / (0.3*1.2*(60)))
 
 
