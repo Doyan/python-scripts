0cb5b0e77ef6239e293d86d5c6b73e3d93e14451
Doyan <gabrielamandus@gmail.com>
2018-09-24 14:12:24 +0200

    k_fitter senaste

--- masterProject/k_fitter.py
+++ masterProject/k_fitter.py
@@ -4,8 +4,8 @@ Created on Thu Sep 20 16:43:30 2018
 
 k_fitter -
 
-Compares solutions of a heat transfer problem with a certain k to 
-a corresponding temperature field from a multiphase simulation 
+Creates 4 dimensional numpy representations of a series of 
+fluent Temperature fields
 
 
 
@@ -32,23 +32,29 @@ times = np.array(times) * tscaling + toffset
 
 
 filename=filelist[230]
-df = pd.read_csv(datapath+filename,names = ['x','y','x1','y1','temp'],skiprows=[0])
+df = pd.read_csv(datapath+filename,names = ['cell','x','y','temp'],skiprows=[0])
 
 # unpack into numpy array
 y = np.array(df.y)
 x = np.array(df.x)
 T = np.array(df.temp)
 
+x0 = 0.0
+x1 = 0.51
+y0 = 0.0
+y1 = 0.4200001
+dcell = 0.015
+
 # create uniform grid axis points
-gx = np.arange(0.0,0.51,0.015)
-gy = np.arange(0.0,0.42,0.015)
+gx = np.arange(x0+dcell/2,x1-dcell/2,dcell)
+gy = np.arange(y0+dcell/2,y1-dcell/2,dcell)
 
 # plot fluent grid for inspection
 plt.plot(x,y,'b.')
 
 plt.plot(gx,np.ones_like(gx)*gy[17],'r.')
-plt.plot(np.ones_like(gy)*gx[14],gy,'y.')
-plt.plot(np.ones_like(gy)*gx[19],gy,'y.')
+plt.plot(np.ones_like(gy)*gx[15],gy,'y.')
+plt.plot(np.ones_like(gy)*gx[18],gy,'y.')
 plt.show()
 
 # make meshgrid from grid axes
@@ -57,26 +63,26 @@ xi, yi = np.meshgrid(gx,gy)
 # interpolate fluent grid onto structured python grid
 
 z0 = griddata((x,y),T,(xi,yi),method='nearest')
-z1 = griddata((x,y),T,(xi,yi),method='linear')
-
-#nanmask = np.isnan(z1)
-#z1[nanmask] = z0[nanmask]    
-
-
 
 # create mask to remove values "inside wall"
 mask = (xi > 0.24) & (xi < 0.27) & (yi > 0.42 - 0.15)
 
-z1[mask] = np.nan
+z0[mask] = np.nan
 
 # plot interpolated grid as surface
-plt.contourf(xi,yi,z0,20,cmap='bwr', vmin=1073.15, vmax=1123.15)
+plt.contourf(xi,yi,z0,10,cmap='bwr', vmin=1073.15, vmax=1123.15)
 
 plt.colorbar()
-#
-#plt.contour(xi,yi,z1,20,colors='black', vmin=1073.15, vmax=1123.15)
-#
-#plt.plot(x,y,'b.')
+
+plt.contour(xi,yi,z0,36,colors='black', vmin=1073.15, vmax=1123.15)
+
+plt.plot(x,y,'b.')
+
+data = [xi,yi,z0,mask]
+
+
+A= np.array((xi,yi,z0))
+
 
 
 #xbar = []
